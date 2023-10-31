e1a14180be758f7a4e178a6585e11fa02f9d0786
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-10-31 00:08:27 -0300

    update ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Helo Word!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Seja bem-vindo(a) ao Sistema Bancário!\n");
+
+        System.out.println("Por favor, informe o número de sua conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Agora, informe o número de sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, informe o seu primeiro nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Olá " + nome +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numeroConta);
     }
 }
