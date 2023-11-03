46c34cef3e50e0e7206c48ad18dbf1cfcbe2297b
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-11-03 16:51:17 -0300

    refactor

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -9,14 +9,23 @@ public class ContaTerminal {
         System.out.println("Por favor, informe o número de sua conta: ");
         int numeroConta = scanner.nextInt();
 
+        scanner.nextLine();
+
         System.out.println("Agora, informe o número de sua agência: ");
-        String agencia = scanner.next();
+        String agencia = scanner.nextLine();
 
         System.out.println("Por favor, informe o seu primeiro nome: ");
-        String nome = scanner.next();
+        String nome = scanner.nextLine();
+
+        System.out.println("Qual o valor do seu depósito inicial? ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
 
         System.out.println("Olá " + nome +
                 ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
-                ", conta " + numeroConta);
+                ", conta " + numeroConta +
+                " e seu saldo R$ " + saldo +
+                " já está disponível para saque.");
     }
 }
